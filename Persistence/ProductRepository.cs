f5e89f08960e4b9952973b903607e47cc9461379
Somad Y <iretibe@users.noreply.github.com>
2020-09-04 10:13:20 +0100

    Created the repository folder called Persistence

---
+++ Persistence/ProductRepository.cs
@@ -0,0 +1,39 @@
+﻿using FlixOne.BookStore.ProductService.Models;
+using System;
+using System.Collections.Generic;
+
+namespace FlixOne.BookStore.ProductService.Persistence
+{
+    public class ProductRepository : IProductRepository
+    {
+        public ProductRepository()
+        {
+
+        }
+
+        public void Add(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Product> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product GetBy(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
